0816b61c747b44cdcaaecc8dc45b404622a6027f
Volker Gropp <volker@gropp.org>
2015-06-26 22:57:40 +0200

    init project setup

---
+++ src/main/java/org/gropp/yatfe/Yatfe.java
@@ -0,0 +1,11 @@
+package org.gropp.yatfe;
+
+/**
+ * Yet another two thousand forty eight
+ *
+ */
+public class Yatfe {
+	public static void main(String[] args) {
+		System.out.println("\nyatfe!\n");
+	}
+}
